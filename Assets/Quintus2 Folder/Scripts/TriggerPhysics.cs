e3f9ba79fa048617b71ada64d153ba962eed288a
qieqs <quintus.glerum@gmail.com>
2024-12-19 10:18:25 +0100

    there you go

--- Assets/Quintus2 Folder/Scripts/TriggerPhysics.cs
+++ Assets/Quintus2 Folder/Scripts/TriggerPhysics.cs
@@ -1,24 +1,32 @@
 using UnityEngine;
 
-public class PickupItem : MonoBehaviour
+public class TriggerPhysics : MonoBehaviour
 {
     public string pickup_Item_string;
     public string letgo_Item_string;
     public GameObject parentBone;
     public GameObject objectToParent;
+    public Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     public void animevent(string s)
     {
         Debug.Log("PrintEvent called at " + Time.time + " with a value of " + s);
-        if(s == pickup_Item_string)
+        if (s == pickup_Item_string)
         {
             Debug.Log("gelukt");
             objectToParent.transform.SetParent(parentBone.transform);
+            rb.isKinematic = true;
         }
-        else if(s == letgo_Item_string)
+        else if (s == letgo_Item_string)
         {
             Debug.Log("laat los");
             objectToParent.transform.parent = null;
+            rb.isKinematic = false;
         }
         else
         {
